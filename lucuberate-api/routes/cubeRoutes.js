9c9ec61ff3a6557e2a81351bc74f0fcb559bb868
khermalik <khermalik@gmail.com>
2020-11-27 18:44:37 -0800

    adds routes for API endpoints

--- lucuberate-api/routes/cubeRoutes.js
+++ lucuberate-api/routes/cubeRoutes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 const ctrl = require('../controllers');
 
+// routes - /api/v1/cubes
+router.get("/", ctrl.cubes.index);
+router.get("/:id", ctrl.cubes.show);
+router.post("/", ctrl.cubes.create);
+router.put("/:id", ctrl.cubes.update);
+router.delete("/:id", ctrl.cubes.destroy);
 
 module.exports = router;
