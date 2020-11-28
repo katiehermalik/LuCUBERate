0927193480a060d52284813012391720a27d2901
khermalik <khermalik@gmail.com>
2020-11-27 18:44:37 -0800

    adds routes for API endpoints

---
+++ lucuberate-api/routes/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  cubes: require("./cubes"),
+};
--- lucuberate-api/server.js
+++ lucuberate-api/server.js
@@ -13,4 +13,6 @@ const corsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 
+app.use("/api/v1/cubes", routes.cubes);
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
