bc00ab303c77f052f704bea30ccef32106dd18a9
khermalik <khermalik@gmail.com>
2020-11-30 16:43:58 -0800

    restructured components

---
+++ lucuberate-client/src/pages/CubeShow.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class CubeShow extends React.Component {
+  state = {
+    cube: {}
+  }
+
+  componentDidMount() {
+
+  }
+
+  renderCube() {
+
+  }
+
+  render() {
+    return(
+      <>
+      <h1>Cube Show Page</h1>
+      </>
+    )
+  }
+}
+
+export default CubeShow;
