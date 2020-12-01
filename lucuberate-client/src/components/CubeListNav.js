ac91235b6a8d2708dfbf92e992b79bc8b9f22eea
khermalik <khermalik@gmail.com>
2020-11-30 19:37:20 -0800

    fixes route change flicker

--- lucuberate-client/src/components/CubeListNav.js
+++ lucuberate-client/src/components/CubeListNav.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CubeList from './CubeList';
+import { Link } from 'react-router-dom';
 
 function Dashboard() {
 
   return(
     <>
       <h4>Cube List Nav</h4>
+      <Link to='/dashboard/new'><button>New Cube</button></Link>
       <CubeList />
     </>
   )
--- lucuberate-client/src/config/routes.js
+++ lucuberate-client/src/config/routes.js
@@ -9,15 +9,11 @@ import CubeShow from '../pages/CubeShow';
 export default (
   <Switch>
     <Route exact path='/' component={ Landing }/>
-    {localStorage.user &&
-    <>
-      <Route exact path='/dashboard' component={ Dashboard }/>
-      <Route exact path='/dashboard/new' component={ CubeNew }/>
-      <Route exact path='/dashboard/index' component={ Dashboard }/>
-      <Route path='/dashboard/:id/edit' component={ CubeEdit }/>
-      <Route path='/dashboard/:id' component={ CubeShow }/>
-    </>
-    }
+    <Route exact path='/dashboard/new' component={ CubeNew }/>
+    <Route exact path='/dashboard/index' component={ Dashboard }/>
+    <Route exact path='/dashboard/:id' component={ CubeShow }/>
+    <Route exact path='/dashboard/:id/edit' component={ CubeEdit }/>
+    <Route exact path='/dashboard' component={ Dashboard }/>
   </Switch>
 )
 
