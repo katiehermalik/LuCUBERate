118f1dad523ef0fdeba5e9e44bf9bc8b7095b0b3
khermalik <khermalik@gmail.com>
2020-12-01 09:17:24 -0800

    re routes logo click

--- lucuberate-client/src/components/Navbar.js
+++ lucuberate-client/src/components/Navbar.js
@@ -15,7 +15,7 @@ class Navbar extends React.Component {
       <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         {this.props.user &&
-          <a className="navbar-brand" href="/">LuCUBErate</a>
+          <a className="navbar-brand" href="/dashboard">LuCUBErate</a>
         }
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
