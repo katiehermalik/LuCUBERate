28114901cf5d68b9516b87783da6b4cdcc43bc64
khermalik <khermalik@gmail.com>
2020-12-01 18:48:40 -0800

    adds lights to scene

--- lucuberate-client/src/components/LandingBackground.js
+++ lucuberate-client/src/components/LandingBackground.js
@@ -1,12 +1,18 @@
 import { Canvas } from "react-three-fiber";
 import {Stars, OrbitControls } from "drei";
 import CubeMesh from "./CubeMesh";
+import Lights from "./Lights";
 
 
 function LandingBackground() {
   return(
-    <Canvas id='canvas'>
+    <Canvas 
+    id='canvas'           
+    shadowMap 
+    colorManagement 
+    camera={{position: [-5, -7, 5], fov: 30}}>
       <CubeMesh />
+      <Lights />
       <Stars 
         radius={150}
         depth={50}
