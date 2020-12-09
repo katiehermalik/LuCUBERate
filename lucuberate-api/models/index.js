3aa462928dcb7b1a994f2b51266a58de25360b45
khermalik <khermalik@gmail.com>
2020-12-08 23:38:30 -0800

    preps server and db connections for deployment

--- lucuberate-api/models/index.js
+++ lucuberate-api/models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://localhost:27017/lucuberate';
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/lucuberate';
 
 const configOptions = {
   useNewUrlParser: true,
