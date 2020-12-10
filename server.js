77468f8c2672b2d3e6078f6a868e031b533e1921
khermalik <khermalik@gmail.com>
2020-12-10 11:54:48 -0800

    fixes cors error

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:3000', 'https://shielded-waters-87290.herokuapp.com/']
 }
 
 app.use(session({
