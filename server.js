1b9b039d4c680a5bbd2f9b0e4cbdf9d516ace7c7
khermalik <khermalik@gmail.com>
2020-12-11 00:44:36 -0800

    fixes file path error

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use("/api", routes.auth);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'lucuberate-client', 'build', 'index.html'));
   });
 }
 
