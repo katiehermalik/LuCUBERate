10c6c01dd6bd6c6304f6e6e95ce8e2adda6511a8
khermalik <khermalik@gmail.com>
2020-12-13 16:36:01 -0800

    fixes flex issues

--- lucuberate-client/src/components/ShowCubePage/StudyCube.js
+++ lucuberate-client/src/components/ShowCubePage/StudyCube.js
@@ -52,33 +52,35 @@ function StudyCube(props) {
         </ul>
         <CubeCtrls cube_id={props.cube_id} />
       </div>
-      <div className="cube-container">
-        <div className={`study-cube ${side || sides[0]}`}>
-          <div className="face Question">
-            <div className={`face-title ${side === 'Question' || side === '' ? '' : 'blur' }`}>Question</div>
-            <div 
-            className={`face-content ${side === 'Question' || side === '' ? '' : 'blur' }`}
-            >{cube.question}</div>
-          </div>
-          <div className="face Link">
-            <div className={`face-title ${side === 'Link' ? '' : 'blur' }`}>Link</div>
-            <div className={`face-content ${side === 'Link' ? '' : 'blur' }`}><a rel="noreferrer" target="_blank" href={cube.link}>{cube.link_alias}</a></div>
-          </div>
-          <div className="face Hint">
-            <div className={`face-title ${side === 'Hint' ? '' : 'blur' }`}>Hint</div>
-            <div className={`face-content ${side === 'Hint' ? '' : 'blur' }`}>{cube.hint}</div>
-          </div>
-          <div className="face Notes">
-            <div className={`face-title ${side === 'Notes' ? '' : 'blur' }`}>Notes</div>
-            <div className={`face-content ${side === 'Notes' ? '' : 'blur' }`}>{cube.notes}</div>
-          </div>
-          <div className="face Visual">
-            <div className={`face-title ${side === 'Visual' ? '' : 'blur' }`}>Visual Aid</div>
-            <img src={cube.visual_aid} alt="random" className={`visual-aid ${side === 'Visual' ? '' : 'blur' }`}/>
-          </div>
-          <div className="face Answer">
-            <div className={`face-title ${side === 'Answer' ? '' : 'blur' }`}>Answer</div>
-            <div className={`face-content ${side === 'Answer' ? '' : 'blur' }`}>{cube.answer}</div>
+      <div className="cube-area">
+        <div className="cube-container">
+          <div className={`study-cube ${side || sides[0]}`}>
+            <div className="face Question">
+              <div className={`face-title ${side === 'Question' || side === '' ? '' : 'blur' }`}>Question</div>
+              <div 
+              className={`face-content ${side === 'Question' || side === '' ? '' : 'blur' }`}
+              >{cube.question}</div>
+            </div>
+            <div className="face Link">
+              <div className={`face-title ${side === 'Link' ? '' : 'blur' }`}>Link</div>
+              <div className={`face-content ${side === 'Link' ? '' : 'blur' }`}><a rel="noreferrer" target="_blank" href={cube.link}>{cube.link_alias}</a></div>
+            </div>
+            <div className="face Hint">
+              <div className={`face-title ${side === 'Hint' ? '' : 'blur' }`}>Hint</div>
+              <div className={`face-content ${side === 'Hint' ? '' : 'blur' }`}>{cube.hint}</div>
+            </div>
+            <div className="face Notes">
+              <div className={`face-title ${side === 'Notes' ? '' : 'blur' }`}>Notes</div>
+              <div className={`face-content ${side === 'Notes' ? '' : 'blur' }`}>{cube.notes}</div>
+            </div>
+            <div className="face Visual">
+              <div className={`face-title ${side === 'Visual' ? '' : 'blur' }`}>Visual Aid</div>
+              <img src={cube.visual_aid} alt="random" className={`visual-aid ${side === 'Visual' ? '' : 'blur' }`}/>
+            </div>
+            <div className="face Answer">
+              <div className={`face-title ${side === 'Answer' ? '' : 'blur' }`}>Answer</div>
+              <div className={`face-content ${side === 'Answer' ? '' : 'blur' }`}>{cube.answer}</div>
+            </div>
           </div>
         </div>
       </div>
