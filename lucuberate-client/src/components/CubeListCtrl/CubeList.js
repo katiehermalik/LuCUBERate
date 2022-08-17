3af391914e8f6795f4492e5b1230da1b263475e7
katiehermlik <katiehermlik@gmail.com>
2022-08-16 17:20:56 -0700

    refactors approach for making an array of refs for cube inputs in CubeList

--- lucuberate-client/src/components/CubeListCtrl/CubeList.js
+++ lucuberate-client/src/components/CubeListCtrl/CubeList.js
@@ -1,48 +1,58 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { withRouter } from 'react-router-dom';
 import CubeContext from '../../context/CubeContextProvider';
 
-  function CubeList (props) {
-    const { cubeList } = useContext(CubeContext);
-    let createdRefs = [];
-    
-    useEffect(() => {
-      if (props.history.location.pathname.match(/\b[\w=.]+$/g)[0] === 'new') {
-        createdRefs.forEach(ref => ref.checked = false);
-      } else {
-        const currentCubeId = props.history.location.pathname.match(/\b[\w=.]+$/g)
-        const foundElement = createdRefs.find(element => element.defaultValue === currentCubeId[0]);
-        if (foundElement) foundElement.checked = true;
-      }
-    }, [createdRefs, props.history.location.pathname])
-    
-    const handleClick = (e) => {
-      props.history.push(`/dashboard/${e.target.value}`)
-    }
+const CubeList = ({ history, history:{location:{pathname}}}) => {
+  const { cubeList } = useContext(CubeContext);
+  const cubeInputRefs = useRef([]);
 
-    const renderNewCubePlaceHolder = () => {
-      if (props.history.location.pathname.match(/\b[\w=.]+$/g)[0] === 'new' && cubeList.cubes) {
-        return(
-          <li className="radio-button">
-            <input
-              type="radio"
-              id={`Cube_${cubeList.cubes.length + 1}`}
-              onChange={handleClick}
-              checked={true}
-              />
-            <label
-              className="radio-label"
-              htmlFor={`Cube_${cubeList.cubes.length + 1}`}>
-                {`Cube ${cubeList.cubes.length + 1}`}
-            </label>
-          </li>
-        )
+  useEffect(() => {
+    if (cubeInputRefs.current.length !== 0 ) {
+      if (pathname.match(/\b[\w=.]+$/g)[0] === 'new') {
+        cubeInputRefs.current.forEach(ref => ref.checked = false);
+        const newestCubeListElement = cubeInputRefs.current[cubeInputRefs.current.length - 1];
+        newestCubeListElement.scrollIntoView({ behavior: 'smooth', block: 'center'});
+      } else {
+        const currentCubeId = pathname.match(/\b[\w=.]+$/g);
+        const foundElement = cubeInputRefs.current.find(element => element.defaultValue === currentCubeId[0]);
+        if (foundElement) {
+          foundElement.checked = true;
+          foundElement.scrollIntoView({behavior: 'smooth', block: 'center'});
+        }
       }
     }
+  }, [cubeList, cubeInputRefs, pathname])
+  
+  const handleClick = (e) => {
+    history.push(`/dashboard/${e.target.value}`);
+    e.target.scrollIntoView({ behavior: 'smooth', block: 'center'});
+  }
+
+  const renderNewCubePlaceHolder = () => {
+    if (pathname.match(/\b[\w=.]+$/g)[0] === 'new' && cubeList.cubes) {
+      return(
+        <li className="radio-button">
+          <input
+            type="radio"
+            id={`Cube_${cubeList.cubes.length + 1}`}
+            onChange={handleClick}
+            checked={true}
+            />
+          <label
+            className="radio-label"
+            htmlFor={`Cube_${cubeList.cubes.length + 1}`}>
+              {`Cube ${cubeList.cubes.length + 1}`}
+          </label>
+        </li>
+      )
+    }
+  }
 
   return (
-    <div className="cube-list container-column cube-select-group">
-      {cubeList.cubes?.map((cube, i) => (
+    <>
+      {cubeInputRefs.current = []}
+      <div className="cube-list container-column cube-select-group">
+        {cubeList.cubes?.map((cube, i) => (
         <li 
           key={`list-item-${cube._id}`}
           className="radio-button"
@@ -54,7 +64,9 @@ import CubeContext from '../../context/CubeContextProvider';
             value={cube._id}
             id={`Cube_${i + 1}`}
             onChange={handleClick}
-            ref={element => {createdRefs.push(element)}}
+            ref={element => { 
+              if (element) cubeInputRefs.current = [...cubeInputRefs.current, element];
+            }}
             />
           <label
             key={`label-item-${cube._id}`}
@@ -67,6 +79,7 @@ import CubeContext from '../../context/CubeContextProvider';
       }  
       {renderNewCubePlaceHolder()}
     </div>
+    </>
   )
 }
 
