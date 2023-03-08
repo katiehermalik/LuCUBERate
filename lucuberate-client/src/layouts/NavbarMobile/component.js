d8da1c67b7dbd3ebd34c2257897851da894add62
katiehermalik <khermalik@gmail.com>
2023-03-07 16:50:32 -0800

    fix: disables 'create cube' link while on 'about' page

--- lucuberate-client/src/layouts/NavbarMobile/component.js
+++ lucuberate-client/src/layouts/NavbarMobile/component.js
@@ -15,21 +15,29 @@ const NavbarMobile = () => {
     <>
       {user && (
         <>
-          <div className="divider-line theme-transition"></div>
-          <nav className="navbar-mobile container-row theme-transition">
+          <div className="divider-line"></div>
+          <nav className="navbar-mobile container-row">
             <SidePanelToggle disable={pathname === "/"} mobileHidden={false} />
             <Link
+              style={{
+                pointerEvents: `${pathname === "/" ? "none" : ""}`,
+                color: `${
+                  pathname === "/"
+                    ? "var(--mobile-nav-item-disabled)"
+                    : params[2] === "new"
+                    ? "var(--mobile-nav-item-select)"
+                    : "var(--text-color-light)"
+                }`,
+              }}
               alt="Create Cube"
-              className={`btn mobile-navbar-item theme-transition ${
-                pathname === "/dashboard/new" ? "selected" : ""
-              }`}
+              className="btn mobile-navbar-item"
               to="/dashboard/new">
               <PlusIcon size={16} />
               Create Cube
             </Link>
             <Link
               alt="About"
-              className={`btn mobile-navbar-item theme-transition ${
+              className={`btn mobile-navbar-item ${
                 pathname === "/" ? "selected" : ""
               }`}
               to="/">
@@ -38,7 +46,7 @@ const NavbarMobile = () => {
             </Link>
             <Link
               alt="Dashboard"
-              className={`btn mobile-navbar-item theme-transition ${
+              className={`btn mobile-navbar-item ${
                 currentPage === "dashboard" && pathname !== "/dashboard/new"
                   ? "selected"
                   : ""
