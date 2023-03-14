54ae25a25dd786172cca2c0a3a0ac0fd584cd7ce
katiehermalik <khermalik@gmail.com>
2023-03-14 12:30:19 -0700

    fix: updates failure redirect and message

--- routes/oauthRoutes.js
+++ routes/oauthRoutes.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const passport = require("passport");
-const {
-  googleSuccessUrl,
-  googleFailedUrl,
-} = require("../config/multi-environment");
+const { googleSuccessUrl, origin } = require("../config/multi-environment");
 
 // /api/v1/oauth
 
@@ -16,8 +13,9 @@ router.get(
   "/google/redirect",
   passport.authenticate("google", {
     successRedirect: googleSuccessUrl,
-    failureRedirect: googleFailedUrl,
-    failureMessage: "Cannot login with Goggle, please try again.",
+    failureRedirect: origin,
+    failureMessage:
+      "Sorry, we couldn't sign you in. It looks like you previously created an account with the same email address you're trying to use to sign in with 'Sign in with Google'. Please try signing in using the email and password you used to create your account. If you continue to experience issues, please contact our support team for assistance.",
   })
 );
 
